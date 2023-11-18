f99e60e6251b5f62437d4ab760819c3e566a40fe
eoinmtreacy <eointreacy5@gmail.com>
2023-11-18 10:43:36 +0000

    game set up loop

--- gui.py
+++ gui.py
@@ -17,7 +17,7 @@ class Input:
         self.color = color
         self.text = ""
         self.font = pygame.font.Font(None, 24)
-        self.img = self.font.render(self.text, True, "red")
+        self.img = self.font.render(self.text, True, self.color)
         self.active = False
 
     def draw(self,screen):
@@ -29,39 +29,57 @@ class Input:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     self.active = False
+                    return self.text
                 elif event.key == pygame.K_BACKSPACE:
                     self.text = self.text[:-1]
                 else:
                     self.text += event.unicode
                 # Re-render the text.
-                self.img = self.font.render(self.text, True, "red")
+                self.img = self.font.render(self.text, True, self.color)
 
 pygame.init()
 
-name_input = Input("red", (HEIGHT-100)/2,( WIDTH-40)/2,100,40)
-stack_input = Input("blue", (HEIGHT-100)/2,( WIDTH-40)/2,100,40)
+name_input = Input("red", (HEIGHT-100)/2,(WIDTH-40)/2,100,40)
+stack_input = Input("blue", (HEIGHT-100)/2,(WIDTH-40)/2,100,40)
 
 screen = pygame.display.set_mode((HEIGHT,WIDTH))
 
-setup, name_input.active = False, True
-game_running = True
+game_running, setup = True, False
+name_input.active = True
 
 while game_running:
     
-    # game set-up: take name and stack size
-    if not setup:
-        screen.fill("grey")
+    # game set-up:
+    while name_input.active or stack_input.active:
+        
+        # enter name
+        while name_input.active:
+            screen.fill("grey")
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_running = False
-            for input in [name_input, stack_input]:
-                input.handle_type(event)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_running = False
+                NAME = name_input.handle_type(event)
 
-        if name_input.active:
-            name_input.draw(screen)
-        # if stack_input.active:
-        #     stack_input.draw(screen)
+            if name_input.active:
+                name_input.draw(screen)
+            
+            pygame.display.update()
+        
+        stack_input.active = True
+        
+        # enter stack
+        while stack_input.active:
+            screen.fill("grey")
 
-        pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_running = False
+                STACK = stack_input.handle_type(event)
+
+            if stack_input.active:
+                stack_input.draw(screen)
+            
+            pygame.display.update()
+
 pygame.quit()
