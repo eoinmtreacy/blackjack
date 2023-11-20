b6b3083b2be16cabd2939bea819d8ea85c086053
eoinmtreacy <eointreacy5@gmail.com>
2023-11-20 15:47:10 +0000

    hit, split stand buttons working

--- objects.py
+++ objects.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 from pygame.locals import *
         
 class Input:
@@ -44,16 +45,31 @@ class Label:
         pygame.draw.rect(screen, self.color, self.rect, 2)
 
 class Button:
-    def __init__(self, name, x, y, w, h, color):
+    def __init__(self, name, x, y, w, h, color, on_click):
         self.name = name
         self.rect = pygame.Rect(x, y, w, h)
         self.color = color
         self.font = pygame.font.Font(None, 24)
         self.img = self.font.render(self.name, True, self.color)
+        self.on_click = on_click
     
     def draw(self, screen):
         screen.blit(self.img, self.rect)
         pygame.draw.rect(screen, self.color, self.rect, 2)
+
+    def handle_click(self):
+        if self.on_click != " ":
+            newevent = pygame.event.Event(pygame.locals.KEYDOWN, unicode=self.on_click, key=pygame.locals.K_a, mod=pygame.locals.KMOD_NONE)
+        else:
+            newevent = pygame.event.Event(pygame.locals.KEYDOWN, unicode=self.on_click, key=pygame.locals.K_RETURN, mod=pygame.locals.KMOD_NONE)
+        pygame.event.post(newevent)
+
+    def process(self):
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                time.sleep(0.25)
+                self.handle_click()                
 
 class Card:
     def __init__(self, suit, value, x, y, w=30, h=50):
